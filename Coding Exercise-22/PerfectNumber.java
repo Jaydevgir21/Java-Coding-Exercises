42bdda6635d8cb1af13683a985d5592bd30bf7ef
Jaydevgir21 <jaydevgirik@gmail.com>
2022-11-08 16:45:46 +0530

    Exercise solution 21 to 24 added

---
+++ Coding Exercise-22/PerfectNumber.java
@@ -0,0 +1,28 @@
+public class PerfectNumber {
+    // write your code here// write your code here// write your code here
+    public static boolean isPerfectNumber(int number) {
+  
+    if(number < 1) {
+     
+     return false; 
+      
+    }
+    int perfect = 0; 
+    for(int i = 1; i < number; i++) {
+    
+      if(number % i == 0) {
+      
+        perfect += i; 
+      
+      }
+      if(perfect == number) {
+      
+        return true; 
+      
+      }
+    
+    }
+    return false;
+  
+  }
+}
